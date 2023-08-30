df59b04a1a5bb1b27a18fbeb97b79a308f0a1ba1
cpp-tutor <cpptutor@outlook.com>
2023-08-30 16:55:03 +0100

    Improve output of expressions

--- visitor/ASTPrinter.hpp
+++ visitor/ASTPrinter.hpp
@@ -25,33 +25,48 @@ static const std::unordered_map<int,std::string_view> ops{
 class ASTPrinter : public ExprVisitor {
 public:
     virtual Value operator()(const ExprAssign& e) const override {
-        return "?";
+        std::string str;
+        str.append("(assign ");
+        str.append(e.getName());
+        str.append(" ");
+        str.append(toString(e.getExpr()->accept(*this)));
+        str.append(")");
+        return str;
     }
 
     virtual Value operator()(const ExprBinary& e) const override {
         std::string str;
-        str += '(';
-        str += ops.find(e.getOp())->second;
-        str += ' ';
-        str += toString(e.getLeft()->accept(*this));
-        str += ' ';
-        str += toString(e.getRight()->accept(*this));
-        str += ')';
+        str.append("(");
+        str.append(ops.find(e.getOp())->second);
+        str.append(" ");
+        str.append(toString(e.getLeft()->accept(*this)));
+        str.append(" ");
+        str.append(toString(e.getRight()->accept(*this)));
+        str.append(")");
         return str;
     }
 
     virtual Value operator()(const ExprCall& e) const override {
-        return "?";
+        std::string str;
+        str.append("(call ");
+        str.append(toString(e.getCallee()->accept(*this)));
+        str.append("()");
+        for (auto expr : e.getArgs()) {
+            str.append(" ");
+            str.append(toString(expr->accept(*this)));
+        }
+        str.append(")");
+        return str;
     }
 
     virtual Value operator()(const ExprGet& e) const override {
-        return "?";
+        return "(get)";
     }
 
     virtual Value operator()(const ExprGrouping& e) const override {
         std::string str{ "(group " };
-        str += toString(e.get()->accept(*this));
-        str += ')';
+        str.append(toString(e.get()->accept(*this)));
+        str.append(")");
         return str;
     }
 
@@ -61,35 +76,39 @@ public:
 
     virtual Value operator()(const ExprLogical& e) const override {
         std::string str;
-        str += '(';
-        str += ops.find(e.getOp())->second;
-        str += ' ';
-        str += toString(e.getLeft()->accept(*this));
-        str += ' ';
-        str += toString(e.getRight()->accept(*this));
-        str += ')';
+        str.append("(");
+        str.append(ops.find(e.getOp())->second);
+        str.append(" ");
+        str.append(toString(e.getLeft()->accept(*this)));
+        str.append(" ");
+        str.append(toString(e.getRight()->accept(*this)));
+        str.append(")");
         return str;
     }
 
     virtual Value operator()(const ExprSet& e) const override {
-        return "?";
+        return "(set)";
     }
 
     virtual Value operator()(const ExprSuper& e) const override {
-        return "super";
+        std::string str;
+        str.append("(super ");
+        str.append(e.get());
+        str.append(")");
+        return str;
     }
 
     virtual Value operator()(const ExprThis& e) const override {
-        return "this";
+        return "(this)";
     }
 
     virtual Value operator()(const ExprUnary& e) const override {
         std::string str;
-        str += '(';
-        str += ops.find(e.getOp())->second;
-        str += ' ';
-        str += toString(e.get()->accept(*this));
-        str += ')';
+        str.append("(");
+        str.append(ops.find(e.getOp())->second);
+        str.append(" ");
+        str.append(toString(e.get()->accept(*this)));
+        str.append(")");
         return str;
     }
 
