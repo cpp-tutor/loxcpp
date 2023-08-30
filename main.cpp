bded4e446a49f7d7b6c087e36fd9ae6d1a982c77
cpp-tutor <cpptutor@outlook.com>
2023-08-30 16:53:34 +0100

    Improve options handling and error reporting

--- main.cpp
+++ main.cpp
@@ -9,6 +9,18 @@
 #include <functional>
 #include <fstream>
 
+#define VER_STR "1.01 (2023-Aug-30)"
+
+#ifdef _MSC_VER
+#define OPTION_SWITCH '/'
+#define USE_STR "[/?|/h|/v] [/i [/a]|/b] [filename]"
+#else
+#define OPTION_SWITCH '-'
+#define USE_STR "[-?|-h|-v] [-i [-a]|-b] [filename]"
+#endif
+
+enum class ReturnCode { Unset = -1, Success = 0, Informational, OptionsError, BadFile, ParseError, RuntimeError };
+
 enum class RuntimeOptions { None = 0, ShowAST = 1, Batch = 2, Interactive = 4, JIT = 8 };
 
 RuntimeOptions operator | (RuntimeOptions lhs, RuntimeOptions rhs) {
@@ -22,11 +34,12 @@ RuntimeOptions operator & (RuntimeOptions lhs, RuntimeOptions rhs) {
 }
 
 int main(const int argc, const char **argv) {
+    auto returnCode = ReturnCode::Unset;
     std::ifstream input{};
     RuntimeOptions opts = RuntimeOptions::None;
     int arg = 0;
     while (++arg < argc) {
-        if (argv[arg][0] == '-') {
+        if (argv[arg][0] == OPTION_SWITCH) {
             switch(argv[arg][1]) {
                 case 'a':
                     opts = opts | RuntimeOptions::ShowAST;
@@ -39,10 +52,19 @@ int main(const int argc, const char **argv) {
                     break;
                 case 'j':
                     opts = opts | RuntimeOptions::JIT;
-                    std::cerr << "Error: JIT engine not yet implemented\n";
-                    return 1;
+                    break;
+                case 'h':
+                case '?':
+                    std::cerr << "Usage: " << argv[0] << ' ' << USE_STR << '\n';
+                    returnCode = ReturnCode::Informational;
+                    break;
+                case 'v':
+                    std::cerr << VER_STR << '\n';
+                    returnCode = ReturnCode::Informational;
                     break;
                 default:
+                    std::cerr << "Unrecognized option \'" << OPTION_SWITCH << argv[arg][1] << "\'\n";
+                    returnCode = ReturnCode::OptionsError;
                     break;
             }
         }
@@ -50,31 +72,60 @@ int main(const int argc, const char **argv) {
             break;
         }
     }
-    if ((opts & (RuntimeOptions::Batch | RuntimeOptions::Interactive))
-        == (RuntimeOptions::Batch | RuntimeOptions::Interactive)) {
-            std::cerr << "Error: Only specify one of -b and -i\n";
-            return 1;
+    if (returnCode != ReturnCode::Informational) {
+        if ((opts & (RuntimeOptions::Batch | RuntimeOptions::Interactive))
+            == (RuntimeOptions::Batch | RuntimeOptions::Interactive)) {
+                std::cerr << "Error: Only specify one of -b and -i\n";
+                returnCode = ReturnCode::OptionsError;
+        }
+        if (((opts & RuntimeOptions::ShowAST) != RuntimeOptions::None)
+            && ((opts & RuntimeOptions::Interactive) == RuntimeOptions::None)) {
+                std::cerr << "Error: Use of -a needs use of -i\n";
+                returnCode = ReturnCode::OptionsError;
+            }
+        if ((opts & RuntimeOptions::JIT) != RuntimeOptions::None) {
+            std::cerr << "Error: JIT engine not yet implemented\n";
+            returnCode = ReturnCode::OptionsError;
+        }
     }
     if ((opts & (RuntimeOptions::Batch | RuntimeOptions::Interactive))
         == RuntimeOptions::None) {
             opts = opts | RuntimeOptions::Batch;
     }
-    if (arg == (argc - 1)) {
-        input.open(argv[arg]);
+    if (arg != argc) {
+        if ((opts & RuntimeOptions::Interactive) != RuntimeOptions::Interactive) {
+            if (arg == (argc - 1)) {
+                input.open(argv[arg]);
+                if (!input.is_open()) {
+                    std::cerr << "Error: Could not open file \'" << argv[arg] << "\'\n";
+                    returnCode = ReturnCode::BadFile;
+                }
+            }
+            else {
+                std::cerr << "Error: Only one input file can be specified\n";
+                returnCode = ReturnCode::OptionsError;
+            }
+        }
+        else {
+            std::cerr << "Error: Cannot specify input file with " << OPTION_SWITCH << "i\n";
+            returnCode = ReturnCode::OptionsError;
+        }
     }
-    std::vector<std::shared_ptr<Stmt>> program;
+    if (returnCode != ReturnCode::Unset) {
+        return static_cast<int>(returnCode);
+    }
     auto environment = Environment::makeShared();
     Interpreter interpreter{ environment };
     Resolver resolver{ interpreter };
-    ASTPrinter print{};
-    std::function<void(std::shared_ptr<Stmt>)> build;
+    std::function<void(std::shared_ptr<Stmt>)> execute;
+    std::vector<std::shared_ptr<Stmt>> program;
     if ((opts & RuntimeOptions::Batch) != RuntimeOptions::None) {
-        build = [&](std::shared_ptr<Stmt> statement){
+        execute = [&](std::shared_ptr<Stmt> statement){
                 program.push_back(statement);
         };
     }
     else {
-        build = [&](std::shared_ptr<Stmt> statement){
+        execute = [&](std::shared_ptr<Stmt> statement){
             statement->accept(resolver);
             statement->accept(interpreter);
             if ((std::dynamic_pointer_cast<StmtExpression>(statement)
@@ -94,17 +145,38 @@ int main(const int argc, const char **argv) {
                         expression = std::dynamic_pointer_cast<StmtVariable>(statement)->getInit();
                     }
                     std::cout << "=> " << toString(interpreter.lastValue())
-                        << ' ' << toString(expression->accept(print)) << '\n';
+                        << ' ' << toString(expression->accept(ASTPrinter{})) << '\n';
                 }
             }
         };
     }
     Loxer scanner{ input.is_open() ? input : std::cin };
-    Loxgram parser{ scanner, &build };
-    if (!parser.parse() && ((opts | RuntimeOptions::Batch) != RuntimeOptions::None)) {
-        for (auto statement : program) {
-            statement->accept(resolver);
-            statement->accept(interpreter);
+    Loxgram parser{ scanner, &execute };
+    if ((opts & RuntimeOptions::Batch) == RuntimeOptions::None) {
+        while (returnCode != ReturnCode::Success) {
+            try {
+                returnCode = parser.parse() ? ReturnCode::ParseError : ReturnCode::Success;
+            }
+            catch (std::exception &e) {
+                std::cerr << e.what() << '\n';
+                returnCode = ReturnCode::RuntimeError;
+            }
         }
     }
+    else {
+        try {
+            returnCode = parser.parse() ? ReturnCode::ParseError : ReturnCode::Success;
+            if (returnCode != ReturnCode::ParseError) {
+                for (auto statement : program) {
+                    statement->accept(resolver);
+                    statement->accept(interpreter);
+                }
+            }
+        }
+        catch (std::exception &e) {
+            std::cerr << e.what() << '\n';
+            returnCode = ReturnCode::RuntimeError;
+        }
+    }
+    return static_cast<int>(returnCode);
 }
